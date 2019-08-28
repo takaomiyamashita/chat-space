43a73f930fe2e28da7c92476673c71fa3476dfa8
takaomiyamashita <ty.8479@gmail.com>
2019-08-28 18:09:57 +0900

    Q4
    completed.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,15 @@
 $(document).on('turbolinks:load', function() {
   $('.new_message').on('submit', function(){
-    console.log("hoge");
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
   });
 });
 
