cd2d85d631dee4d93a0f4aab2c793be185c5d0ae
takaomiyamashita <ty.8479@gmail.com>
2019-08-28 16:45:23 +0900

    Q3
    completed.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,8 @@
-$('form__submit').on('submit', function(){
-  console.log(hoge);
+$(document).on('turbolinks:load', function() {
+  $('.new_message').on('submit', function(){
+    console.log("hoge");
+  });
 });
+
+// １行目のコード記載することで、以下のネストの内容が動作するようになるらしい。
+// form_forメソッドでイベント発火させたい場合は、new_messageをクラスに指定する。これは暗記。
