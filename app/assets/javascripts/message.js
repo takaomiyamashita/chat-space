a8f77267fea3d26682f5a09859c535ce73376cb0
takaomiyamashita <ty.8479@gmail.com>
2019-08-29 12:20:35 +0900

    Q8~11
    completed.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -54,8 +54,14 @@ $(document).on('turbolinks:load', function() {
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);      
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       $('form')[0].reset();
     })
+      .fail(function(){
+        alert('error');
+      });
+      return false;
+    });
   });
 });
 
