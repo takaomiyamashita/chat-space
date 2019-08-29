e41bdf74aaf77a6269a1f54594728166e7241478
takaomiyamashita <ty.8479@gmail.com>
2019-08-29 12:07:51 +0900

    Q7,8
    completed.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,43 @@
+function buildHTML(message){
+  if ( message.image ) {
+    var html =
+     `<div class="message" data-message-id=${message.id}>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.date}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+        <asset_path src=${message.image} >
+      </div>`
+    return html;
+  } else {
+    var html =
+     `<div class="message" data-message-id=${message.id}>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.date}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+    return html;
+  };
+}
 $(document).on('turbolinks:load', function() {
   $('.new_message').on('submit', function(){
     e.preventDefault();
@@ -10,8 +50,29 @@ $(document).on('turbolinks:load', function() {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);      
+      $('form')[0].reset();
+    })
   });
 });
 
-// １行目のコード記載することで、以下のネストの内容が動作するようになるらしい。
-// form_forメソッドでイベント発火させたい場合は、new_messageをクラスに指定する。これは暗記。
+// $(document).on('turbolinks:load', function() {
+//   $('.new_message').on('submit', function(){
+//     e.preventDefault();
+//     var formData = new FormData(this);
+//     var url = $(this).attr('action')
+//     $.ajax({
+//       url: url,
+//       type: "POST",
+//       data: formData,
+//       dataType: 'json',
+//       processData: false,
+//       contentType: false
+//    });
+// });
+
+// // １行目のコード記載することで、以下のネストの内容が動作するようになるらしい。
+// // form_forメソッドでイベント発火させたい場合は、new_messageをクラスに指定する。これは暗記。
