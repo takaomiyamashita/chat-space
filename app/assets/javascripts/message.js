8c9a6a95d80d9932b77036412de8dd3bb99ad5ab
takaomiyamashita <ty.8479@gmail.com>
2019-08-29 19:14:46 +0900

    非同期通信

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,7 +39,8 @@ function buildHTML(message){
   };
 }
 $(document).on('turbolinks:load', function() {
-  $('.new_message').on('submit', function(){
+  $('.new_message').on('submit', function(e){
+    console.log(ok);
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -57,28 +58,13 @@ $(document).on('turbolinks:load', function() {
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       $('form')[0].reset();
     })
-      .fail(function(){
-        alert('error');
-      });
+    .fail(function(){
+      alert('error');
+    })
+    .always(function(){
+      $(".form__submit").prop('disabled', faise);
+    });
       return false;
     });
   });
 });
-
-// $(document).on('turbolinks:load', function() {
-//   $('.new_message').on('submit', function(){
-//     e.preventDefault();
-//     var formData = new FormData(this);
-//     var url = $(this).attr('action')
-//     $.ajax({
-//       url: url,
-//       type: "POST",
-//       data: formData,
-//       dataType: 'json',
-//       processData: false,
-//       contentType: false
-//    });
-// });
-
-// // １行目のコード記載することで、以下のネストの内容が動作するようになるらしい。
-// // form_forメソッドでイベント発火させたい場合は、new_messageをクラスに指定する。これは暗記。
