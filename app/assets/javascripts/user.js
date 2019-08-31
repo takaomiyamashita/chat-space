baea203fa3091cf67f470b86022d5821a0d9ccad
takaomiyamashita <ty.8479@gmail.com>
2019-08-31 19:41:27 +0900

    Based on the value of asynchronous communication, it was reflected in HTML.
    非同期通信の値を元に、HTMLに反映しました。

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -31,3 +31,25 @@ $('#user-search-field').on('keyup', function(e){
     alert('ユーザー検索に失敗しました');
 });
 });
+
+$(document).on('turbolinks:load', function(){
+
+  var search_list = $("#user-search-result");
+  var member_list = $("#member-append");
+
+  function appendUser(user){
+      var html = 
+                  `<div class="chat-group-user clearfix">
+                      <p class="chat-group-user__name">${user.name}</p>
+                      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
+                  </div>`;
+                  search_list.append(html);
+  }
+
+  function appendErrMsgToHTML(msg){
+      var html = 
+                  `<div class="chat-group-user clearfix">
+                      <p class="chat-group-user__name">${msg}</p>
+                  </div>`;
+                  search_list.append(html);
+  }
