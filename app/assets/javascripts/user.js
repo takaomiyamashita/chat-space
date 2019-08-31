7a182ce7c5a32d1e5d57d95e9aadbcbe3cecdc40
takaomiyamashita <ty.8479@gmail.com>
2019-08-31 19:32:36 +0900

    Asynchronous communication was enabled at the event.
    イベント時に非同期通信できるようにした

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,2 +1,33 @@
 $('#user-search-field').on('keyup', function(e){
   var input = $("#user-search-field").val();
+
+  $.ajax({
+    type: 'GET',                
+    url:  '/users',             
+    data: { keyword: input},    
+    dataType: 'json'            
+})
+
+.done(function(users){                
+
+    if (input.length === 0) {        
+        $('#user-search-result').empty();
+      }
+
+    else if (input.length !== 0){     
+        $('#user-search-result').empty();
+        users.forEach(function(user){ 
+            appendUser(user)
+        });
+    }
+
+    else {
+        $('#user-search-result').empty();
+        appendErrMsgToHTML("一致するユーザーが見つかりません");
+    }
+})
+
+.fail(function() {
+    alert('ユーザー検索に失敗しました');
+});
+});
