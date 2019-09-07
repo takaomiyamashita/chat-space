3dc587ea127f79b292bb6df7290395d778f2de40
takaomiyamashita <ty.8479@gmail.com>
2019-09-07 13:18:04 +0900

    微修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,10 +12,10 @@ set :repo_url,  'git@github.com:takaomiyamashita/chat-space.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['ty8479.pem'],
+set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/ty8479.pem'] 
 
 # プロセス番号を記載したファイルの場所
