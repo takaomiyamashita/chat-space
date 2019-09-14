d4610c0a5129a5a9d876d3302c0f063f216589c1
takaomiyamashita <ty.8479@gmail.com>
2019-09-14 20:25:02 +0900

    animateメソッドの一部記述変更

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -33,8 +33,8 @@ $(document).on('turbolinks:load', function() {
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.messages').append(html);      
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 1500);
+      $('.new_message').append(html);      
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 5000);
       $('form')[0].reset();
     })
 
